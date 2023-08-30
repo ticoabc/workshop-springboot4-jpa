1581f812244112a656902c7215e559d2a6a87886
Tiago de Freitas <tiagoabc@gmail.com>
2023-08-30 17:19:53 -0300

    Created User insert

--- src/main/java/com/educandoweb/course/ressources/UserRessource.java
+++ src/main/java/com/educandoweb/course/ressources/UserRessource.java
@@ -1,13 +1,17 @@
 package com.educandoweb.course.ressources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.services.UserService;
@@ -19,17 +23,25 @@ public class UserRessource {
 	@Autowired
 	private UserService service;
 	
-	//Metedo teste para acessar os usuários
+	//Metedo teste (endpoints) para acessar todos os usuários
 	@GetMapping
 	public ResponseEntity<List<User>> findAll(){
 		List<User> list = service.findAll();		
 		return ResponseEntity.ok().body(list);
 	}
 	
-	
+	//Metedo teste (endpoints) para acessar os usuários pelo id
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<User> findByid(@PathVariable Long id){
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);		
-	}	
+	}
+	
+	//Metedo (endpoints) para inserir um usuário
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);	
+	}
 }
