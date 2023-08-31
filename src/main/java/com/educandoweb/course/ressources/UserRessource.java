6a23d3bad58525ff20e4928bc83157d06303be51
Tiago de Freitas <tiagoabc@gmail.com>
2023-08-31 07:45:25 -0300

    Created User Update

--- src/main/java/com/educandoweb/course/ressources/UserRessource.java
+++ src/main/java/com/educandoweb/course/ressources/UserRessource.java
@@ -3,12 +3,15 @@ package com.educandoweb.course.ressources;
 import java.net.URI;
 import java.util.List;
 
+import javax.print.DocFlavor.READER;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,4 +55,21 @@ public class UserRessource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+	
+	//Metedo (endpoints) para deletar um usuário
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj){
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
